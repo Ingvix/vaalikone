701b5c448bc8cf33e553d7f42e872c316f12ed90
Erik Kosonen <erik.kosonen@student.hamk.fi>
2020-04-13 12:42:51 +0300

    lisää alkuperäinen vaalikone

---
+++ src/main/java/Ohjauspaneeli/Ohjauspaneeli.java
@@ -0,0 +1,28 @@
+package Ohjauspaneeli;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/Ohjauspaneeli"}
+)
+public class Ohjauspaneeli extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Hello App Engine!\r\n");
+    
+    
+
+  }
+}
