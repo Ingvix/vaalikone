b9df7b2f5cfab3007135143747ee2c5d593f2482
Tuomas1814 <tuomas1814@student.hamk.fi>
2020-04-14 15:03:20 +0300

    Kysymysten lisääminen
    Ohjauspaneeli.html tiedostossa action="loginServlet" muutettu action="UusiKysymys".
    
    Lisätty uusi luokka UusiKysymys.java joka lisää käyttäjän syöttämän kysymyksen tietokantaan.

---
+++ src/main/java/Ohjauspaneeli/UusiKysymys.java
@@ -0,0 +1,42 @@
+package Ohjauspaneeli;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import persist.Kysymykset;
+
+@WebServlet("/UusiKysymys")
+public class UusiKysymys extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	// read form fields
+		String kysymys = request.getParameter("kysymys");
+		
+	      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "vaalikones" );
+	      
+	      EntityManager entitymanager = emfactory.createEntityManager( );
+	      entitymanager.getTransaction( ).begin( );
+	      
+	      Kysymykset kysymykset = new Kysymykset( );
+	      kysymykset.setKysymys( kysymys );
+	      
+	      entitymanager.persist( kysymykset );
+	      entitymanager.getTransaction( ).commit( );
+
+	      entitymanager.close( );
+	      emfactory.close( );
+		
+	}
+
+}
